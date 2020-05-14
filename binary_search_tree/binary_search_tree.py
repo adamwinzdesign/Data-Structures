8d99aafe7d1c2b59468dd6de3786b11a045ae50f
adamwinzdesign <adamwinzdesign@gmail.com>
2020-05-14 16:04:08 -0500

    updated for each

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,7 +17,22 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        node = self
+        def find_node(node):
+            if value < node.value:
+                if node.left is None:
+                    node.left = BSTNode(value)
+                    # if value has been assigned, end recursion
+                    return True
+                else:
+                    return find_node(node.left)
+            else:
+                if node.right is None:
+                    node.right = BSTNode(value)
+                    return True
+                else:
+                    return find_node(node.right)
+        return find_node(node)
 
     # Return True if the tree contains the value
     # False if it does not
@@ -40,11 +55,23 @@ class BSTNode:
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # if there is no self.right, return the current value
+        # if there is a self.right, go to that node and check again until there is no self.right, then return the current value as above.
+        while self.right is not None:
+            self = self.right
+        return self.value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        # call the fn on the value at this node 
+        fn(self.value)
+
+        # pass this function to the left child 
+        if self.left:
+            self.left.for_each(fn)
+        # pass this function to the right child 
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
@@ -75,13 +102,36 @@ class BSTNode:
         pass
 
 
+# from collections import deque
+
 # class BSTNode:
 #     def __init__(self, value):
 #         self.value = value
 #         self.left = None
 #         self.right = None
-		
-# 	# Return True if the tree contains the value
+
+#     # Insert the given value into the tree
+#     def insert(self, value):
+#         # check if the incoming value is less than the current node's value 
+#         if value < self.value:
+#             # we know we need to go left 
+#             # how do we know when we need to recurse again, 
+#             # or when to stop? 
+#             if not self.left:
+#                 # we can park our value here 
+#                 self.left = BSTNode(value)
+#             else:
+#                 # we can't park here 
+#                 # keep searching 
+#                 self.left.insert(value)
+#         else:
+#             # we know we need to go right 
+#             if not self.right:
+#                 self.right = BSTNode(value)
+#             else:
+#                 self.right.insert(value)
+
+#     # Return True if the tree contains the value
 #     # False if it does not
 #     def contains(self, target):
 #         # when we start searching, self will be the root
@@ -101,3 +151,92 @@ class BSTNode:
 #             if not self.right:
 #                 return False
 #             return self.right.contains(target)
+
+#     # Return the maximum value found in the tree
+#     def get_max(self):
+#         # we'll keep going right until there are no more nodes on the right side 
+#         if not self.right:
+#             return self.value
+#         # otherwise, keep going right 
+#         return self.right.get_max()
+
+#     def iterative_get_max(self):
+#         current_max = self.value 
+
+#         current = self
+#         # traverse our structure
+#         while current is not None:
+#             if current.value > current_max:
+#                 current_max = current.value
+#             # update our current_max variable if we see a larger value 
+#             current = current.right
+
+#         return current_max
+
+#     # Call the function `fn` on the value of each node
+#     # Do we expect a return from the for_each function? No 
+#     def for_each(self, fn):
+#         # call the fn on the value at this node 
+#         fn(self.value)
+
+#         # pass this function to the left child 
+#         if self.left:
+#             self.left.for_each(fn)
+#         # pass this function to the right child 
+#         if self.right:
+#             self.right.for_each(fn)
+        
+
+#     def iterative_for_each(self, fn):
+#         stack = [] 
+
+#         # add the root node
+#         stack.append(self)
+
+#         # loop so long as the stack still has elements 
+#         while len(stack) > 0:
+#             current = stack.pop()
+#             if current.right:
+#                 stack.append(current.right)
+#             if current.left:
+#                 stack.append(current.left)
+
+#             fn(current.value)
+
+#     # Depth-First traversal 
+#     # LIFO ordering of the tree elements 
+
+#     def breadth_first_for_each(self, fn):
+#         queue = deque()
+
+#         # add the root node
+#         queue.append(self)
+
+#         # loop so long as the stack still has elements 
+#         while len(queue) > 0:
+#             current = queue.popleft()
+#             if current.left:
+#                 queue.append(current.left)
+#             if current.right:
+#                 queue.append(current.right)
+
+#             fn(current.value)
+
+
+# import random
+
+# v1 = random.randint(1, 101)
+# v2 = random.randint(1, 101)
+# v3 = random.randint(1, 101)
+# v4 = random.randint(1, 101)
+# v5 = random.randint(1, 101)
+
+# bst = BSTNode(5)
+
+# bst.insert(v1)
+# bst.insert(v2)
+# bst.insert(v3)
+# bst.insert(v4)
+# bst.insert(v5)
+
+# bst.for_each(lambda v: print(v))
